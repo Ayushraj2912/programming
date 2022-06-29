a67ea099b0e2f8005c3da1f0e1b10206e7bf02e0
Ayush raj <107629635+Ayushraj2912@users.noreply.github.com>
2022-06-29 18:31:27 +0530

    Update postiveOrNegativeNumber.c

--- postiveOrNegativeNumber.c
+++ postiveOrNegativeNumber.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
  
 int main()
@@ -11,20 +10,4 @@ int main()
         printf("%d is a positive number \n", num);
     else if (num < 0)
         printf("%d is a negative number \n", num);
-    
-=======
-#include <stdio.h>
- 
-int main()
-{
-    int num;-
- 
-    printf("Enter a number: \n");
-    scanf("%d", &num);
-    if (num > 0)
-        printf("%d is a positive number \n", num);
-    else if (num < 0)
-        printf("%d is a negative number \n", num);
-    
->>>>>>> b3e9bcd5ab9951dcbddf413a37b1a6cc888c30f4
 }
